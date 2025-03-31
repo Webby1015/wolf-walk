14275bf8f1a08fe3f3247c7a1a7a79eca8b2ae53
webby1015 <vedantabhishek@gmail.com>
2025-04-01 03:11:19 +0530

    Options Layout

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -1,5 +1,9 @@
 extends Control
 
 
+
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+func _on_back_mouse_entered() -> void:
+	$HoverSound.play()
