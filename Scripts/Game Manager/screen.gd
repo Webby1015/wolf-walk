10b64e6371abbd4a13929c599b2cfbd62a56960a
webby1015 <vedantabhishek@gmail.com>
2025-04-01 18:44:09 +0530

    solved more resolutions issue

--- Scripts/Game Manager/screen.gd
+++ Scripts/Game Manager/screen.gd
@@ -6,6 +6,12 @@ var ResolutionList = {}
 func getScreenResolutions() -> Dictionary:
 	var resolution = DisplayServer.screen_get_size()
 	ResolutionList[0] = [resolution.x, resolution.y]
+	for i in range(commonResolutions.size()):
+		if commonResolutions[i] == [resolution.x, resolution.y]:
+			continue
+		else:
+			ResolutionList[i+1] = commonResolutions[i]
+
 	return ResolutionList
 	
 func changeResolution(x:int,y:int) -> void:
